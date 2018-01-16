491c1e7e2a42790cf507f3b2786c741f355c602b
zoneix <zoneix@gmail.com>
2018-01-16 15:09:17 -0500

    adding template

---
+++ app-old.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var myvalue = process.env.CLIENTID,
+	myvalue2 = process.env.CLIENTSEC,
+	myvalue3 = process.env.VAR2;
+
+app.get('/', function(req, res) {
+  res.send({
+	  "myvalue3": myvalue3,
+	  "myvalue2": myvalue2,
+	  "myvalue" : myvalue
+  });
+  //console.log (process.env.MYVALUE);
+});
+
+app.get('/keepalive', function(req, res) {
+	var timestamp = Date.now();
+	  res.send({
+		  timestamp
+	  });
+	  console.log (process.env.MYVALUE);
+	});
+
+app.post('/', function(req, res) {
+  res.set('Content-Type', 'text/html');
+  res.send({
+    "Output": "process.env.MYVALUE"
+  });
+  res.send('<p>some html here</p>');
+});
+
+
+// Export your Express configuration so that it can be consumed by the Lambda handler
+module.exports = app
